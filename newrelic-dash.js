ea6540020550bd67dbdaea774c76d5fe3d269f34
cheekymohnkey <ryan.clarke.nz@outlook.com>
2019-06-18 14:04:46 +1200

    Update newrelic-dash.js

--- newrelic-dash.js
+++ newrelic-dash.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Kragle SLI Dashboard Script
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://one.newrelic.com/launcher/dashboards.launcher*
